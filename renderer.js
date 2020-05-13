33cbcb541671eb78f4c320750508bab7eec221f5
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-05-12 21:27:07 -0400

    Added scope switching

--- renderer.js
+++ renderer.js
@@ -5,6 +5,10 @@
 //   myNotification.onclick = () => {
 //     console.log('Notification clicked')
 //   }
+var $ = require("jquery");
+$(document).ready( () => {
+  console.log("Ready")
+});
 
 const currency = new Intl.NumberFormat('en-US',
   { style: 'currency', currency: 'USD',
@@ -12,8 +16,10 @@ const currency = new Intl.NumberFormat('en-US',
 const weeksInMonth = 4.35;
 const weeksInYear = 52;
 
-document.querySelector("#btnCalc").onclick = () => {calcAndDisplay()};
-
+// document.querySelector("#btnCalc").onclick = () => {calcAndDisplay()};
+$("#btnCalc").click(() => {calcAndDisplay()});
+// document.querySelector("#scopeChoice").onchange = () => {switchScope()};
+$("#scopeChoice").change(() => {switchScope()});
 function calcAndDisplay() {
   //set gross weekly val to (rate*hours) * (100-tax)/100
   let afterTax = ((100 - document.querySelector("#tax").value) / 100);
@@ -38,9 +44,31 @@ function calcAndDisplay() {
   document.querySelector("#grossYr").innerHTML = currency.format(grossYear);
   document.querySelector("#grossYrAT").innerHTML = currency.format(grossYearAftTax);
 }
+function switchScope() { 
+  let target = $("#outputGrid");
+  console.log("trigger");
+  console.log(target);
+  var show = $("#scopeChoice").prop("selectedIndex");
+  $(target).children().addClass("hide");
+  switch (show) {
+    case 0:
+      $(".outputWk").removeClass("hide");
+      break;
+    case 1:
+      $(".outputMn").removeClass("hide");
+      break;
+    case 2:
+      $(".outputYr").removeClass("hide");
+      break;
+    default:
+      $(target).children().removeClass("hide");
+      break;
+  }
+}
 
 function sumExpenses() {
-
+  let expenses = 0;
+  return expenses;
 }
 
 function addExpense() {

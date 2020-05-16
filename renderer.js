8f8cfee39bd778dfea200aae143469d77b644a5d
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2020-05-16 12:09:48 -0400

    Continue Expense work

--- renderer.js
+++ renderer.js
@@ -7,7 +7,8 @@
 //   }
 
 class Expense {
-  constructor(name, amount) {
+  constructor(expid, name, amount) {
+    this.expid = expid;
     this.name = name;
     this.amount = amount;
   }
@@ -21,14 +22,25 @@ const weeksInYear = 52;
 
 var $ = require("jquery");
 var expenses = [];
+var expenseIndex = 0;
 
 $(document).ready( () => {
   console.log("Ready")
 });
 $("#btnAdd").click(() => {addExpense()});
+$("#btnAdd").click(() => {rmSelectedExpenses()});
 $("#btnCalc").click(() => {calcAndDisplay()});
 $("#scopeChoice").change(() => {switchScope()});
-
+$(".btnRemove").click((e) => {
+  //this target is an img inside of a btn
+  console.log("target is:" + e.target.parentNode.name);
+  rmExpense(e);
+})
+$(".expenseField").change((event) => {
+  //this target is a text input
+  console.log("target is:" + event.target.name);
+  updateExpData(event);
+})
 function calcAndDisplay() {
   //set gross weekly val to (rate*hours) * (100-tax)/100
   let afterTax = ((100 - $("#tax").val()) / 100);
@@ -74,6 +86,7 @@ function switchScope() {
 }
 
 function sumExpenses() {
+  console.log("@ sumExpenses()")
   let expSum = 0
   expenses.forEach((expense) => {
     expSum += expense.amount;
@@ -85,15 +98,40 @@ function sumExpenses() {
 
 function addExpense() {
   console.log("@ addExpense()")
-  expenses.push(new Expense("Gasoline", "500"));
-  sumExpenses();
+  //determine id (first 3 letter of name + index)
+  // at this point it will be unk0
+  //create div.expenseItem
+  //create input:text.expenseField name=id+"-name" placeholder="name"
+  //create input:number.expenseField name=id+"-amount" placeholder="amount"
+  //create btn name=id
+  //expenses.push(new Expense("unk0","unknown", "0"));
+  //sumExpenses();
+}
+
+function updateExpData(event) {
+  //get target name from event
+  //parse out the ID
+  
+  //if name field
+  //  
+  //else
+  //  
+  //if target dne in expenseList
+  //
+  //  expenses.push(new)
 }
 
 function rmSelectedExpenses() {
-
+  console.log("@ rmSelectedExpenses()")
 }
-
+function rmExpense(event) {
+  console.log("@ rmExpense()")
+}
 function save() {
+  console.log("@ save()")
   console.log("not done");
+  //if there are expenses w/out names -> dont include
+  //if w/out amounts -> use 0
 }
+
 

e3d212de69d67ead46ff6af2b6febefb10ff3c63
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-04-19 14:43:22 -0400

    add customized app menu template

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
 const path = require("path");
 const { save, load } = require("./fileHandler");
 
 let win;
 function createWindow() {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 1000,
     height: 600,
@@ -16,43 +15,28 @@ function createWindow() {
     },
   });
 
-  // and load the index.html of the app.
-  // win.loadFile('index.html')
-  //
   // if production/packaged version
   if (process.env.NODE_ENV === "production") {
     win.loadFile("build/index.html");
   } else {
     win.loadURL("http://localhost:3000");
   }
-  // Open the DevTools.
-  // win.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 ipcMain.on("load", (event, args) => {
   console.log(`Got "${args}" from renderer`);
@@ -70,3 +54,57 @@ ipcMain.on("save", (event, args) => {
   }
   win.webContents.send("save", res);
 });
+
+const isMac = process.platform === "darwin";
+
+const template = [
+  { role: "appMenu" },
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New",
+        accelerator: process.platform === "darwin" ? "Cmd+n" : "Ctrl+n",
+        click: () => {
+          console.log("Clicked menu New");
+        },
+      },
+      { type: "separator" },
+      {
+        label: "Open...",
+        accelerator: process.platform === "darwin" ? "Cmd+o" : "Ctrl+o",
+        click: () => {
+          console.log("Clicked menu open");
+        },
+      },
+      { type: "separator" },
+      {
+        label: "Save",
+        accelerator: process.platform === "darwin" ? "Cmd+s" : "Ctrl+s",
+        click: () => {
+          console.log("Clicked menu save");
+        },
+      },
+      { type: "separator" },
+      isMac ? { role: "close" } : { role: "quit" },
+    ],
+  },
+  { role: "editMenu" },
+  { role: "viewMenu" },
+  { role: "windowMenu" },
+  {
+    role: "help",
+    submenu: [
+      {
+        label: "Learn More",
+        click: async () => {
+          const { shell } = require("electron");
+          await shell.openExternal("https://github.com/liampalmer0/MoneyMan");
+        },
+      },
+    ],
+  },
+];
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
